7c320a3665e89ab210c1631cce937c21460e6f67
Michemor <yatoijema@gmail.com>
2023-06-15 12:21:41 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,25 @@
+#include<unistd.h>
+#include<string.h>
+void errQuote(char*);
+/**
+ * main - Entry point
+ * 
+ * Return: 1
+ */
+int main(void)
+{
+char* quoteMsg="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+errQuote(quoteMsg);
+return (1);
+}
+
+/**
+ * errQuote - prints message to stderr
+ * @*quote: First operand
+ *
+ */
+void errQuote(char* quote)
+{
+write(2,quote,strlen(quote));
+}
+
