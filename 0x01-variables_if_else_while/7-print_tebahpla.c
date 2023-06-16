c8032d69a60b959290d0ba78a955360a5fa72b57
Michemor <yatoijema@gmail.com>
2023-06-16 13:49:10 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int rev = 122;
+while (rev >= 97)
+{
+putchar(rev);
+rev--;
+}
+putchar('\n');
+return (0);
+}
