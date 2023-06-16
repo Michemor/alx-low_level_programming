19aef4de6aa557d9b20358137bbfac4ae682fd3c
Michemor <yatoijema@gmail.com>
2023-06-16 13:44:00 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int nums = 48;
+while (nums <= 57)
+{
+putchar(nums);
+nums++;
+}
+putchar('\n');
+return (0);
+}
+
