cd68789fec278fd0bc7525abdeb2750f0ea3bc84
Michemor <yatoijema@gmail.com>
2023-06-16 05:59:29 -0700

    resolved betty style of coding errors

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -11,15 +11,16 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n>0)
+if(n > 0)
 {
-printf("%d is positive\n",n);
-}else if (n==0)
+printf("%d is positive\n", n);
+}
+else if (n == 0)
 {
-printf("%d is zero\n",n);
+printf("%d is zero\n", n);
 }else
 {
-printf("%d is negative\n",n);
+printf("%d is negative\n", n);
 }
-return 0;
+return (0);
 }
