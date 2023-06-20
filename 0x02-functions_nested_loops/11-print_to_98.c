95890eaf6167370283dadb7420389095314e95b6
Michemor <yatoijema@gmail.com>
2023-06-20 09:19:44 -0700

    Correction of "11. 98 Battery Street, the OG"

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,7 @@
 void print_to_98(int n)
 {
 int i;
-if(n <= 98 )
+if (n <= 98)
 {
 for (i = n; i <= 97; i++)
 {
@@ -18,7 +18,7 @@ printf("98\n");
 }
 else
 {
-for (i = n; i >= 97; i--)
+for (i = n; i >= 98; i--)
 {
 printf("%d, ", i);
 }
