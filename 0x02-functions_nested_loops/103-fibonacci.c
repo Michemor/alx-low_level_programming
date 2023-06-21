09898f01a7f02bb0f6f98c88460fee23d3d7a6f7
Michemor <yatoijema@gmail.com>
2023-06-21 12:59:30 -0700

    1. Like, comment, subscribe - correction

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,21 +10,22 @@ long int m = 1;
 long int n = 2;
 long int sum = 0;
 int count;
+long int tot_sum = 0;
 for (count = 1; count <= 1000; count++)
 {
-if (m < 4000000 &&  m % 2 == 0)
-{
-printf("%ld, ", m);
-}
-else
+if (m > 4000000)
 {
 break;
 }
+if (m % 2 == 0)
+{
 sum = m + n;
+tot_sum += sum;
 m = n;
 n = sum;
 }
-printf("\n");
+}
+printf("%ld\n", tot_sum);
 return (0);
 }
 
