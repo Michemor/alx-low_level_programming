fbe02bcf336f5dbfefdbc576bf39bb506a18b43e
Michemor <yatoijema@gmail.com>
2023-06-23 11:43:24 -0700

    Task 5 reviwed and checked

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,13 @@ for (count = 0; count < 10; count++)
 {
 for (num = 0; num <= 14; num++)
 {
-_putchar(num + '0');
-if(num >= 9)
+if (num > 9)
 {
-_putchar((num / 10) + (num % 10) + '0');
+_putchar((num / 10) + '0');
 }
+_putchar((num % 10) + '0');
 }
 _putchar('\n');
 }
-_putchar('\n');
 }
 
