baeca89d0e689674332c42fa8d43ba4bbd3a9f96
Michemor <yatoijema@gmail.com>
2023-06-26 06:32:34 -0700

    Correction of "6. Half the lies they tell about me aren't true"

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,21 +8,17 @@ void puts2(char *str)
 {
 int i, len;
 len = strlen(str);
-for (i = 0; i <= len; i++)
-{
 if (len == 0)
 {
-break;
+_putchar(92);
 }
 else
+{
+for (i = 0; i <= len; i++)
 {
 if (i % 2 == 0)
 {
 _putchar(*(str + i));
-}
-else
-{
-continue;
 }
 }
 }
