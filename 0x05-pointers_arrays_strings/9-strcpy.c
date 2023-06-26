5e06f09b871b29269405b88eb915af1014d0dfbb
Michemor <yatoijema@gmail.com>
2023-06-26 09:39:43 -0700

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+/**
+ * *_strcpy - copies one string to another
+ * @des: string to be copied to
+ * @src: the string from which is to be copied
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (src);
+}
