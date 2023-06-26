01c75dd5d79f0d69116113f4881a3c9ee529209d
Michemor <yatoijema@gmail.com>
2023-06-26 10:43:41 -0700

    added len + 1 to the length of the string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,16 +7,16 @@
 void puts_half(char *str)
 {
 int i, len, n;
-len = strlen(str);
+len = strlen(str) + 1;
 if (len > 0)
 {
-if (len % 2 != 0)
+if (len % 2 == 0)
 {
-n = (len - 1) / 2;
+n = len / 2;
 }
 else
 {
-n = len / 2;
+n = (len - 1) / 2;
 }
 for (i = n; i < len; i++)
 {
