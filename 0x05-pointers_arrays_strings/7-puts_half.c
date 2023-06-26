acb27f31a38fea98f44d57287626214a36db6b47
Michemor <yatoijema@gmail.com>
2023-06-26 09:57:32 -0700

    removed option for empty characters

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,12 +8,6 @@ void puts_half(char *str)
 {
 int i, len, n;
 len = strlen(str);
-if (len <= 0)
-{
-_putchar(' ');
-}
-else
-{
 if (len % 2 != 0)
 {
 n = (len - 1) / 2;
@@ -27,5 +21,4 @@ for (i = n; i < len; i++)
 _putchar(*(str + i));
 }
 _putchar('\n');
-}
 }
