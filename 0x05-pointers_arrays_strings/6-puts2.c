21b9e2eed26877f5686a7ec4d388717b52df231f
Michemor <yatoijema@gmail.com>
2023-06-26 06:20:36 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: string 
+ */
+void puts2(char *str)
+{
+int i, len;
+len = strlen(str);
+for (i = 0; i <= len; i++)
+{
+if (i % 2 == 0)
+{
+_putchar(*(str + i));
+}
+else
+{
+continue;
+}
+}
+_putchar('\n');
+}
