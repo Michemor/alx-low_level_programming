c9fe2993ecb0a2a28df999b325257fd22e36f9ed
Michemor <yatoijema@gmail.com>
2023-06-26 09:01:51 -0700

    8. Arrays are not pointers - if block for 0 is left out

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,10 +9,16 @@ void print_array(int *a, int n)
 {
 int i;
 int size = n - 2;
+if (size <= 0)
+{
+
+}
+else
+{
 for (i = 0; i <= size; i++)
 {
 printf("%d, ", *(a + i));
 }
 printf("%d\n", *(a + (n - 1)));
 }
-
+}
