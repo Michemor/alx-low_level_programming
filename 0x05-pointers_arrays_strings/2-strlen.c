1fd04f7ecaee01027edd8ae00bffb431cec2b046
Michemor <yatoijema@gmail.com>
2023-06-26 04:20:20 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - finds the length of a given string
+ * @s: the string whose length is to be found
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
