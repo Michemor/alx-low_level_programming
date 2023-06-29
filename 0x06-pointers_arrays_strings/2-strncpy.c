e85fea608a785964ff34ce97b6a4c8c1d738ced1
Michemor <yatoijema@gmail.com>
2023-06-29 03:01:03 -0700

    strncpy - copies n characters from src to dest string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strncpy - copies src to dest
+ * @dest: string to be copied to
+ * @src: string to be copied
+ * @n: number of characters to be copied
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+strncpy(dest, src, n);
+return (dest);
+}
