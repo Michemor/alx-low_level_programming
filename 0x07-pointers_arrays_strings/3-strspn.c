40034e4a45687019d0f08ed6d2c1868fdee4aa64
Michemor <yatoijema@gmail.com>
2023-07-03 15:30:03 -0700

    removed trailing white spaces

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,11 @@
 #include <string.h>
 #include "main.h"
 /**
- * _strspn - finds number of characters that matches pattern in accept to s
- * @s: target string
- * @accept: string containing pattern to be searched
+ *_strspn - finds number of characters that matches pattern in accept to s
+ *@s: target string
+ *@accept: string containing pattern to be searched
  *
- * Return: number of characeters in s which consists of only characters contained in accept
+ *Return: number of characeters in s which consists of only characters contained in accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
