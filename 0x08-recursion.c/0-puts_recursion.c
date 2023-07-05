7e782d3d355c339d46d1a5acbe8e2e0034e4bb55
Michemor <yatoijema@gmail.com>
2023-07-05 04:07:01 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion.c/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _puts_recursion - prints string to be printed to the screen
+ * @s: string to be printed
+ */
+void _puts_recursion(char *s)
+{
+if (strlen(s) == 0)
+{
+_putchar('\n');
+}
+else
+{
+_putchar(s[0]);
+_puts_recursion(s + 1);
+}
+}
