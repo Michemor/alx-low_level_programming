ad512fe9043a48a551ea0111d0fe3b86f3b0f48e
Michemor <yatoijema@gmail.com>
2023-07-05 07:55:38 -0700

    script that calculates the power of a number to another

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _pow_recursion - prints x raised to the power of y
+ * @x: base
+ * @y: exponent
+ *
+ * Return: x^y
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+{
+return (1);
+}
+else
+{
+return (x * _pow_recursion(x, y - 1));
+}
+}
