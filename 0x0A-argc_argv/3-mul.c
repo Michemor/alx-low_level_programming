7539aeb5d4fd4ed1cd5774c63f057109f3a15b52
Michemor <yatoijema@gmail.com>
2023-07-07 08:40:10 -0700

    3. Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiploes two arguments passed to it
+ * @argc: number of arguments passed
+ * @argv: argument vector
+ *
+ * Return: 1 or 0
+ */
+int main(int argc, char **argv)
+{
+int i, a, b, mult = 1;
+if (argc > 1)
+{
+for (i = 0; i < argc; i++)
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+}
+mult = a * b;
+printf("%d\n", mult);
+return (0);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
