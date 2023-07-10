5be01b1028d78c37bc2692357f0684a1b4108432
Michemor <yatoijema@gmail.com>
2023-07-10 13:48:46 -0700

    3. If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * alloc_grid - dynamically creates 2D array
+ * @width: width of the array
+ * @height: height of the array
+ *
+ * Return: pointer to the array
+ */
+int **alloc_grid(int width, int height)
+{
+int i, j, r;
+int **grid;
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+grid = malloc(sizeof(int *) * height);
+if (grid == NULL)
+{
+return (NULL);
+}
+else
+{
+for (r = 0; r < height; r++)
+{
+grid[r] = malloc(sizeof(int) * width);
+}
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+grid[i][j] = j * 0;
+}
+putchar('\n');
+}
+return (grid);
+}
+}
