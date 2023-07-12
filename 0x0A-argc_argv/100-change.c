80e3f9232487e40f01c4ad933c3cc4efc6be7e20
Michemor <yatoijema@gmail.com>
2023-07-12 11:28:39 -0700

    removed trailing whitespace

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints change for given amount passed as argument 
+ * main - prints change for given amount passed as argument
  * @argc: argument count
  * @argv: argument vector
  *
