26f2747837f22e3b70f57cbea83fc2bf298d272a
Michemor <yatoijema@gmail.com>
2023-07-13 14:17:12 -0700

    correction added loop that popullates new pointer with content

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,36 +9,43 @@
  * Return: pointer to memory
  */
 void *_realloc(void *ptr, unsigned int new_size, unsigned int old_size)
+{ 
+char *nmem;
+int nsize, i;
+if (new_size > old_size)
+{
+return (ptr);
+}
+if (new_size == old_size)
+{
+return (ptr);
+}
+if (ptr == NULL)
 {       
-        void *nmem;
-        int nsize;
-        if (new_size > old_size)
-        {       
-                return (ptr);
-        }       
-        if (new_size == old_size)
-        {       
-                return (ptr);
-        }       
-         if (ptr == NULL)
-        {       
-                nmem = malloc(new_size);
-		if (nmem == NULL)
-			return (NULL);
-                return (nmem);
-                free(nmem);
-        }       
-        if (new_size == 0 && ptr != NULL)
-        {       
-                return (NULL);
-                free(ptr);
-        }
-		nsize = old_size + new_size;
-		nmem = realloc(ptr, nsize);
-		if (nmem == NULL)
-			return (NULL);
-		return (nmem);
-		free(nmem);
-	
+nmem = malloc(new_size);
+if (nmem == NULL)
+{
+return (NULL);
+}
+return (nmem);
+free(nmem);
+}       
+if (new_size == 0 && ptr != NULL)
+{
+return (NULL);
+free(ptr);
+}
+nsize = old_size + new_size;
+nmem = realloc(ptr, nsize);
+if (nmem == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < nsize; i++)
+{
+nmem[i] = ((char *)ptr)[i];
+}
+return (nmem);
+free(nmem);	
 }
 
