303fc17c398bfd402fc6109e5e992b06d0b11254
Michemor <yatoijema@gmail.com>
2023-07-17 10:34:21 -0700

    made changes to code

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,14 +14,17 @@ if ((d->name) == NULL)
 {
 	printf("Name: (nil)");
 }
-else if ((d->owner) == NULL)
+else
+{
+	printf("Name: %s\n", d->name);
+}
+printf("Age: %f\n", d->age);
+if (d->owner ==  NULL)
 {
 	printf("Owner: (nil)");
 }
 else
 {
-printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
-printf("Owner: %s\n", d->owner);
+	printf("Owner: %s\n", d->owner);
 }
 }
