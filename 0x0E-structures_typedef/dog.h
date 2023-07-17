aaa36221874a31f4a7a22ff5f0ac8dc38e45f4b3
Michemor <yatoijema@gmail.com>
2023-07-17 12:10:05 -0700

    rewrote header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,27 @@
-#pragma once
+#ifndef _DOG_H_
+#define _DOG_H_
+
 /**
- * struct dog - stores infomation about dog
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * struct dog - basic qualities of dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
  */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 
 /**
- * dog_t - an alias for struct dog
+ * dog - typedef of struct dog
  */
 typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+#endif /* _DOG_H_ */
