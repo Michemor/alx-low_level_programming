53273792084abe3c148c3633ec5cd763b80c2831
Michemor <yatoijema@gmail.com>
2023-07-18 06:38:57 -0700

    stored strings of name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,6 +10,9 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, len = 0;
+	char *maxName;
+	char *maxOwner;
 	dog_t *max;
 
 	max = malloc(sizeof(dog_t));
@@ -18,14 +21,47 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(max);
 		return (NULL);
 	}
-	else
+	for (i = 0; name[i]; i++)
 	{
-	max->name = name;
-	max->age = age;
-	max->owner = owner;
-	return (max);
-	free(name);
-	free(owner);
-	free(max);
+		len++;
 	}
+	maxName = malloc(sizeof(char) * (len + 1));
+	if (maxName == NULL)
+	{
+		free(maxName);
+		return (NULL);
+	}
+        
+	len = 0;
+	for(i = 0; owner[i]; i++)
+	{
+		len++;
+	}
+	maxOwner = malloc(sizeof(char) * (len + 1));
+	if (maxOwner == NULL)
+	{
+		free(maxOwner);
+		return (NULL);
+	}
+
+	max->name = _strcpy(maxName, name);
+	max->age = age;
+	max->owner = _strcpy(maxOwner, owner);
+
+	return (max);
 }
+char *_strcpy(char *dest, char *src)
+{
+	int i, len = 0;
+	
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+		len++;
+	}
+        dest[len] = '\0';
+	return (dest);
+}
+
