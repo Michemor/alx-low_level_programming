43e09a1d6140e77098157648aa86ea79372da0ee
Michemor <yatoijema@gmail.com>
2023-07-19 03:01:29 -0700

    file that prints name using callback function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints name using callback function
+ * @name: name to be printed
+ * @f: callback function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
