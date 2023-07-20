106fd5ad3c7e4f0855dd9555e48d2b667a7ce119
Michemor <yatoijema@gmail.com>
2023-07-20 14:45:59 -0700

    resolved betty coding style issues and NULL separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,17 +11,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	
 	va_start(ap, n);
-	if (separator && n != 0)
-	{
 	for (i = 0; i < n; i++)
 	{
 	printf("%d", va_arg(ap, int));
-	if (i != (n - 1))
+	if (i != (n - 1) && separator != NULL)
 	{
 	printf("%s", separator);
-	}
 	}
 	}
 	printf("\n");
