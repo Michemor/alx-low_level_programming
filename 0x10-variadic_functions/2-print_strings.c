6539b365923f28279167bb0cf8cea35db674d014
Michemor <yatoijema@gmail.com>
2023-07-20 07:43:03 -0700

    betty style of coding errors resolved

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,29 +7,33 @@
  * @n: number of strings
  */
 void print_strings(const char *separator, const unsigned int n, ...)
-{       
-        unsigned int i;
+{
+	unsigned int i;
 	char *str;
-        va_list sp;
+	va_list ap;
 
-        va_start(sp, n);
-        if (separator)
-        {       
-        i = 0;
-        while(i < n)
-        {       
-        str = va_arg(sp, char*);
+	va_start(ap, n);
+	if (separator)
+	{
+	i = 0;
+	while (i < n)
+	{
+	str = va_arg(ap, char *);
 	if (str == NULL)
 	{
 	printf("(nil)");
 	}
+	else
+	{
 	printf("%s", str);
+	}
 	if (i != (n - 1))
 	{
 	printf("%s", separator);
 	}
 	i++;
 	}
-	}
 	printf("\n");
-}                       
+	}
+	va_end(ap);
+}
