e561d31a859b11a221ef06d460f45059825994d4
Michemor <yatoijema@gmail.com>
2023-07-27 21:20:08 +0000

    file uses assembly language to print statement in main

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+extern printf
+section .data
+	msg: db "Hello, Holberton",0
+	fmt: db  "%s",10,0
+section .text
+global main
+
+main:
+	push rbp
+	mov rsi,msg
+	mov rdi,fmt
+	mov rax,0
+	call printf
+	pop rbp
+	mov rax,0
+	ret
