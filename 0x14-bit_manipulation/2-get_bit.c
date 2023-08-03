e71e0779b0d4b0ae04454f3c61085479a16f1f6b
Michemor <yatoijema@gmail.com>
2023-08-03 20:19:08 +0000

    file gets the bit at a specific position

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * get_bit - obtains bit at specific index from integer n
+ * @n: decimal to be checked
+ * @index: specific bit to be obtained
+ *
+ * Return: bit at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	char bit_n;
+	
+	if (index > 32)
+		return (-1);
+
+	bit_n = n >> index;
+
+	if ((bit_n & 1) >= 1)
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
