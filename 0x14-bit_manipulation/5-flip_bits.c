bb6c86424111c0ab61a872cc4332e11d94f5f614
Michemor <yatoijema@gmail.com>
2023-08-03 22:20:56 +0000

    counts number of flips that need to be made for one number to be equal to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - flips the bits of n and m 
+ * @n: first integer
+ * @m: second integer
+ *
+ * Return: number of bits to be flipped for n to be equal to m
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count;
+	unsigned int res = 0;
+
+	res = n ^ m;
+
+	count = 0;
+	while (res > 0)
+	{
+	res &= (res - 1);
+	count++;
+	}
+	return (count);
+}
