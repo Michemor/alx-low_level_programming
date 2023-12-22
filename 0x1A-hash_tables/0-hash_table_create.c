4e1796d2d351f2b51d1eb5ade34834cfe36e99cc
Michemor <yatoijema@gmail.com>
2023-12-22 14:34:00 +0300

    made changes to allocating memory

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,10 +13,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	/* allocate memory for the table*/
 	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		return (NULL);
+	}
 	table->size = size;
 	/* allocate memory for the array of elements */
-	table->array = malloc(size  * (sizeof(hash_node_t *)));
-	for (i = 0; i < table->size; i++)
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
 	{
 		table->array[i] = NULL;
 	}
