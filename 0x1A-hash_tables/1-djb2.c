34930d3844bc4b404f0b1824b93d6bae29696f58
Michemor <yatoijema@gmail.com>
2023-12-22 14:42:53 +0300

    hash function uses djb2 algorithm to assign index to hash function

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - uses the djb2 algorithm to allocate an index to
+ * the str which is a key
+ * @str: key to be added to the hash table
+ *
+ * Return: an integer indicating the index of the str
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; 
+		/* hash * 33 + c */
+	}
+	return (hash);
+}
