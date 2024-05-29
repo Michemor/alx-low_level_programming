7bee9c11160edf5a083c3759702761c680089c3c
Michemor <yatoijema@gmail.com>
2024-05-30 01:46:43 +0300

    5. Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+island_perimeter(grid):
+returns the perimeter of the island described in grid
+ 
+
+grid:list of list of integers:
+        - 0 represents a water zone
+        - 1 represents a land zone
+        - One cell is a square with side length 1
+        - Grid cells are connected horizontally/vertically (not diagonally).
+        - Grid is rectangular, width and height don’t exceed 100
+"""
+
+def island_perimeter(grid):
+    """
+    grid:list of list of integers:
+        - 0 represents a water zone
+        - 1 represents a land zone
+        - One cell is a square with side length 1
+        - Grid cells are connected horizontally/vertically (not diagonally).
+        - Grid is rectangular, width and height don’t exceed 100
+    """
+
+    return (12)
+
